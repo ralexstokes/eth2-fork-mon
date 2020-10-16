7cd96ed415eac998c84dd557696bd9b7dce88a21
Alex Stokes <r.alex.stokes@gmail.com>
2020-10-16 14:24:12 -0700

    add missing file via gitignore shenanigans

---
+++ cmd/eth2-fork-mon/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ralexstokes/eth2-fork-mon/pkg/monitor"
+
+	"gopkg.in/yaml.v2"
+)
+
+var configFile = flag.String("config-file", "", "path to configuration")
+
+func main() {
+	flag.Parse()
+
+	configFile, err := os.Open(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decoder := yaml.NewDecoder(configFile)
+	config := &monitor.Config{}
+	err = decoder.Decode(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	forkMonitor := monitor.FromConfig(config)
+
+	err = forkMonitor.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
